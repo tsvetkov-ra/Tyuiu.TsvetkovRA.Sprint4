93cc2176b74cf95b1b13f3727669e9711f32fc97
Admin <artykleshine@gmail.com>
2025-11-12 20:19:25 +0300

    Task1

---
+++ Tyuiu.TsvetkovRA.Sprint4.Task1.V19.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.TsvetkovRA.Sprint4.Task1.V19.Lib;
+
+namespace Tyuiu.TsvetkovRA.Sprint4.Task1.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 1, 4, 5, 3, 5, 4, 4, 6, 2, 1, 1, 2 };
+            int res = ds.Calculate(numsArray);
+            int wait = 22;
+            Assert.AreEqual(res, wait);
+
+        }
+    }
+}
---
+++ Tyuiu.TsvetkovRA.Sprint4.Task1.V19/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.TsvetkovRA.Sprint4.Task1.V19.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла for                                                *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #19                                                             *");
+        Console.WriteLine("* Выполнил : Цветков Роман Андреевич | ИИПб-25-1                          *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* подсчитать сумму четных элементов массива, вводимого с клавиатуры       *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        
+        int len;
+        len = Convert.ToInt32(Console.ReadLine());
+        int[] numsArray = new int[len];
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine("ВВЕДИТЕ ЗНАЧЕНИЕ" + i + "ЭЛЕМЕНТА МАССИВА:");
+            numsArray[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate(numsArray));
+    }
+}
